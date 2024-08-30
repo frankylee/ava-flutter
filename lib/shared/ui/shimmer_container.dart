444fa77497bea7f7ed9f4080686a2f17c25c7d6b
frankylee <frankyleekelly@gmail.com>
2024-08-30 16:46:03 -0700

    feat: add shimmer pub dependency and ShimmerContainer widget
    This facilitates code reuse by defining a ShimmerContainer that accepts height and width to create a shimmer box for loading states.
    
    <!-- ps-id: adcf9868-81c6-4654-89fd-9426b4c8df1d -->

---
+++ lib/shared/ui/shimmer_container.dart
@@ -0,0 +1,36 @@
+import 'package:ava_flutter/shared/extension/build_context_ext.dart';
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class ShimmerContainer extends StatelessWidget {
+  const ShimmerContainer({
+    super.key,
+    required this.height,
+    required this.width,
+  });
+
+  final double height;
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return Shimmer.fromColors(
+      baseColor: context.colors.onPrimary,
+      highlightColor: context.colors.primaryContainer,
+      child: Column(
+        children: [
+          Container(
+            height: height,
+            width: width,
+            decoration: ShapeDecoration(
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(4.0),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
