b507a50995f436b63f82666c1a96ed23a1e1e9f1
frankylee <frankyleekelly@gmail.com>
2024-08-30 03:29:53 -0700

    chore: handle FlutterError and PlatformDispatcherError types
    This facilitates the logging of errors of the application.
    
    This adds the logging and intl pub dependencies.
    
    <!-- ps-id: 8674bddd-ce02-4f4b-b2b5-be6c68fc4021 -->

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,48 @@
+import 'dart:developer';
+
 import 'package:ava_flutter/app/ava.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
+import 'package:logging/logging.dart';
 
 void main() {
-  runApp(const Ava());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize the Logging package.
+  Logger.root.level = Level.CONFIG;
+  Logger.root.onRecord.listen((record) {
+    final dateFormatter = DateFormat.jms();
+    log(
+      '[${dateFormatter.format(record.time)}] ${record.message}',
+      level: record.level.value,
+      name: record.loggerName,
+      time: record.time,
+    );
+  });
+  final logger = Logger('Ava');
+
+  // Handle all Flutter framework errors.
+  FlutterError.onError = ((details) {
+    if (details.silent) return;
+    logger.severe(
+      '[Flutter Error] ${details.exceptionAsString()}',
+      details.exception,
+      details.stack,
+    );
+  });
+
+  // Handle all Platform errors not caught by the framework.
+  PlatformDispatcher.instance.onError = (error, stack) {
+    logger.severe('[Platform Dispatcher Error] $error', error, stack);
+    return true;
+  };
+
+  // Run application with Riverpod by wrapping the app in ProviderScope.
+  runApp(
+    const ProviderScope(
+      child: Ava(),
+    ),
+  );
 }
