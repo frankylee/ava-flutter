d3c110badc6fa35fdaf715a22ae08fcefbbade95
frankylee <frankyleekelly@gmail.com>
2024-08-31 15:28:32 -0700

    feat: add SecureAccount Async Notifier view model
    This facilitates the retrival and caching of the SecureAccount details.
    
    This will be used to interact with the Secure Account and will ultimately be displayed on the Home Page. This simulates an API request with a delay.
    
    <!-- ps-id: a9f2ac7a-f41f-4c4e-b2cf-1deb6bc95dcc -->

---
+++ lib/entities/secure_account/view_model/secure_account_notifier.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+
+import 'package:ava_flutter/entities/secure_account/model/secure_account.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class SecureAccountAsyncNotifier extends AsyncNotifier<SecureAccount> {
+  static final provider =
+      AsyncNotifierProvider<SecureAccountAsyncNotifier, SecureAccount>(
+    SecureAccountAsyncNotifier.new,
+  );
+
+  @override
+  FutureOr<SecureAccount> build() async {
+    await Future.delayed(const Duration(milliseconds: 2000));
+    return const SecureAccount(
+      balance: 30.0,
+      creditLimit: 600.0,
+      spendLimit: 100.0,
+      totalBalance: 8390.0,
+      totalLimit: 200900.0,
+      utilization: 0.04,
+    );
+  }
+}
